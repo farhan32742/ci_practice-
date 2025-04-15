336c2e5179452b5125c2ea9fe2f2b0efd5260581
farhan327426 <farhanfayaz1987@gmail.com>
2025-04-15 10:09:33 +0500

    first commit

---
+++ ci_pracice.py
@@ -0,0 +1,15 @@
+def addation():
+    a = 5
+    b = 10
+    c = a + b
+    print("The sum of", a, "and", b, "is:", c)
+    return c
+
+
+def subtraction():
+    a = 10
+    b = 5
+    c = a - b
+    print("The difference of", a, "and", b, "is:", c)
+    return c
+
